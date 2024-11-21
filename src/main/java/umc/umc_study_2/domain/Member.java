50b8b0f57d633572a8480bb8f27f97b55c218948
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-11-22 03:58:28 +0900

    feat: 8주차 실습 완료 (api&swagger)

--- src/main/java/umc/umc_study_2/domain/Member.java
+++ src/main/java/umc/umc_study_2/domain/Member.java
@@ -2,6 +2,9 @@ package umc.umc_study_2.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.umc_study_2.domain.common.BaseEntity;
 import umc.umc_study_2.domain.enums.Gender;
 import umc.umc_study_2.domain.enums.MemberStatus;
@@ -16,6 +19,8 @@ import java.util.List;
 
 @Entity
 @Getter
+@DynamicUpdate
+@DynamicInsert
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -53,6 +58,7 @@ public class Member extends BaseEntity {
     private String email;
 
     @Column(nullable = false)
+    @ColumnDefault("0")
     private Integer point;
 
     @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
