ef3e777d7584a6d99a50e1d673bbcaf38cc1b409
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-11-21 19:57:38 +0900

    init: 프로젝트 생성

---
+++ src/main/java/umc/umc_study_2/UmcStudy2Application.java
@@ -0,0 +1,13 @@
+package umc.umc_study_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmcStudy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/umc/umc_study_2/UmcStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.umc_study_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcStudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
