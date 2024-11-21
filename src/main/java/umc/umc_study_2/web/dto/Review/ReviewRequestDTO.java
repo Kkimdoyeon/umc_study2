76baa32f375a9ab74f94834294ecce0fcde9edaf
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-11-22 06:07:31 +0900

    feat: 어노테이션 추가 적용

--- src/main/java/umc/umc_study_2/web/dto/Review/ReviewRequestDTO.java
+++ src/main/java/umc/umc_study_2/web/dto/Review/ReviewRequestDTO.java
@@ -3,12 +3,13 @@ package umc.umc_study_2.web.dto.Review;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import umc.umc_study_2.validation.annotation.ExistMember;
 import umc.umc_study_2.validation.annotation.ExistStore;
 
 public class ReviewRequestDTO {
     @Getter
     public static class addReviewDTO {
-        @NotNull
+        @ExistMember
         Long memberId;
         @ExistStore
         Long storeId;
