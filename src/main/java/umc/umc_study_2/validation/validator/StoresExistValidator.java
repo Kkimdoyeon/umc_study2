972069f165198bb23ccc5ffd10858e6e86c0e2da
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-11-22 05:12:44 +0900

    refactor: annotation명 변경

--- src/main/java/umc/umc_study_2/validation/validator/StoresExistValidator.java
+++ src/main/java/umc/umc_study_2/validation/validator/StoresExistValidator.java
@@ -6,15 +6,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.umc_study_2.apiPayload.code.status.ErrorStatus;
 import umc.umc_study_2.service.StoreService.StoreQueryService;
-import umc.umc_study_2.validation.annotation.ExistStores;
+import umc.umc_study_2.validation.annotation.ExistStore;
 
 @Component
 @RequiredArgsConstructor
-public class StoresExistValidator implements ConstraintValidator<ExistStores, Long> {
+public class StoresExistValidator implements ConstraintValidator<ExistStore, Long> {
     private final StoreQueryService storeQueryService;
 
     @Override
-    public void initialize(ExistStores constraintAnnotation) {
+    public void initialize(ExistStore constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
--- src/main/java/umc/umc_study_2/web/dto/Mission/MissionRequestDTO.java
+++ src/main/java/umc/umc_study_2/web/dto/Mission/MissionRequestDTO.java
@@ -3,14 +3,14 @@ package umc.umc_study_2.web.dto.Mission;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import umc.umc_study_2.validation.annotation.ExistStores;
+import umc.umc_study_2.validation.annotation.ExistStore;
 
 import java.time.LocalDate;
 
 public class MissionRequestDTO {
     @Getter
     public static class AddMissionDTO {
-        @ExistStores
+        @ExistStore
         Long storeId;
         @NotBlank
         String mission_spec;
